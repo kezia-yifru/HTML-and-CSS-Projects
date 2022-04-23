dbe7464d4f21d2b06ca1dfd707f08682ee4eed04
kezia-yifru <yifrukezia@gmail.com>
2022-04-23 13:23:33 -0700

    Push to git

---
+++ Basic_HTML_and_JavaScript_1/Basic_Javascript_1.js/Hello_World.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<title>Hi there</title>
+	<meta charset="utf-8">
+	</head>	
+<script>
+alert("Hello, World! I am an alert box!");
+</script>
+</html>
+	
+	
