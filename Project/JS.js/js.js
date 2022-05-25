a8f520ad4f2702661b08c0a85051299820758734
kezia-yifru <yifrukezia@gmail.com>
2022-05-24 20:22:55 -0700

    commit

--- Project/JS.js/js.js
+++ Project/JS.js/js.js
@@ -14,4 +14,26 @@ function validateForm() {
   function closeForm() {
     document.getElementById("myForm").style.display = 
     "none";
-  }
+  } 
+
+  $("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow');
+}, 3000); 
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow');
+}, 3000);
